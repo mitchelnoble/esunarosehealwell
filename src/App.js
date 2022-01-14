4288c9bc8dabc0d338e5ac1b07b2c862cd322041
mitchel noble <mitchelparkernoble@gmail.com>
2022-01-14 15:48:09 -0800

    screen folder has placeholder files for each screen, files are imported to app.js, and app has been routed in index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Landing from "./screens/Landing";
+import About from "./screens/About";
+import Schedule from "./screens/Schedule";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Landing />
+      <About />
+      <Schedule />
     </div>
   );
 }
