05e347c229ecb72acdf8ee2741ddaf1383053941
mitchel noble <mitchelparkernoble@gmail.com>
2022-04-10 13:33:03 -0700

    renamed screens to sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./screens/Header";
-import Menu from "./screens/Menu";
-import About from "./screens/About";
-import Schedule from "./screens/Schedule";
+import Header from "./sections/Header";
+import Menu from "./sections/Menu";
+import About from "./sections/About";
+import Schedule from "./sections/Schedule";
 
 function App() {
   return (
---
+++ src/sections/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>Katherine Golez</h1>
+      <h3>Massage Therapist & Wellness Educator</h3>
+    </>
+  );
+};
+
+export default About;
